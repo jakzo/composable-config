a675bb7bf6bebf811c32aaf664b0f095168367eb
Jack Field <jfield@atlassian.com>
2018-10-16 00:00:29 +1100

    clarify type comments

--- src/io/ct.ts
+++ src/io/ct.ts
@@ -5,14 +5,14 @@ import { addConvertor } from './util';
 /* tslint:disable:variable-name */
 
 /**
- * Accepts an integer between 1 and 65535 inclusive.
+ * Accepts an integer in the form of a number or string between 1 and 65535 inclusive.
  * Returns a number.
  */
 export const Port = t.refinement(Integer, n => n >= 1 && n <= 0xffff, 'Port');
 
 /**
- * Accepts a JSON string or an object.
- * Returns an object.
+ * Accepts a JSON string or a JSON value (null, boolean, number, string, array, object).
+ * Returns a JSON value.
  */
 export const Json = addConvertor(
   t.union([t.null, t.number, t.boolean, t.string, t.object], 'Json'),
